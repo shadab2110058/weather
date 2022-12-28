d8b59017094ee7a2d0d2ee0143f4ccc3e062565c
shadab2110058 <shadab2110058@akgec.ac.in>
2022-12-28 10:15:46 +0530

    changes

--- src/Components/Front.js
+++ src/Components/Front.js
@@ -12,7 +12,7 @@ const Front = () => {
   const getWeatherDetails = (cityName) => {
     const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=" + apiKey + ""
     axios.get(apiURL).then((res) => {
-      // console.log(res);
+      console.log(res);
       setData(res.data)
     }).catch((e)=>{
       console.log(e);
@@ -38,22 +38,24 @@ const Front = () => {
   return (
     <div className='full'>
       <div className='container'>
-        <div>
+        <div className='weather'>
           <div className='cloud'><FaCloud size='8%' color='#4d99e9' /></div>
           <h1>Weather App</h1>
         </div>
-        <input type="text" placeholder='Enter city' onChange={handleInputChange}></input>
+        <div>
+        <input type="text" placeholder='Enter city' onChange={handleInputChange}></input></div>
         
-        <div className='button'><button onClick={handleSearch}> search</button></div>
+        <div className='button'><button  onClick={handleSearch}> search</button></div>
         <h2 className='live-weather-condition'>Live Weather Condition</h2>
         <h2 className='temp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C</h2>
-        <h2 className='cityName'>{data?.name}</h2>
+        <h2 className='cityName'>{data?.name},{data?.sys?.country}</h2>
         <div className='max'>
-          <div className='min'> <h3 className='max-temp'>Max:{((data?.main?.temp_max) - 273.15).toFixed(2)}°C</h3></div>
-          <div className='min'> <h3 className='max-temp'>Min:{((data?.main?.temp_min) - 273.15).toFixed(2)}°C</h3></div>
-          <div className='min'> <h3 className='max-temp'>Hum:{data?.main?.humidity}%</h3></div>
-        </div>
+          <div className='min'> <h3 className='max-temp'>Max_temp:<br></br>{((data?.main?.temp_max) - 273.15).toFixed(2)  }°C</h3></div>
+          <div className='min'> <h3 className='max-temp'>Min_temp:<br></br>{((data?.main?.temp_min) - 273.15).toFixed(2)}°C</h3></div>
+          <div className='min'> <h3 className='max-temp'>humidity:<br></br>{data?.main?.humidity}%</h3></div>
+          <div className='min'> <h3 className='max-temp'>Speed:<br></br>{data?.wind?.speed}m/s</h3></div>
       </div>
+    </div>
     </div>
   )
 }
