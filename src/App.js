75974ea769e3036079fe95a9f223002c749fc3b4
shadab2110058 <shadab2110058@akgec.ac.in>
2023-07-09 11:57:08 +0530

    useeffectbremove

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Front from './Components/Front'
+import LoadingBar from 'react-top-loading-bar';
 import "./App.css"
+import { useState } from 'react';
 
 const App = () => {
+
+  const [progress,setProgress] = useState(0);
+
   return (
     <div>
-      <Front/>
+      <LoadingBar 
+      color='#e11e50'
+      height={10}
+      progress={progress}
+      />
+      <Front setProgress={setProgress}/>
     </div>
+
   )
 }
 
--- src/Components/Front.js
+++ src/Components/Front.js
@@ -1,39 +1,41 @@
 import axios from 'axios';
 import { FaCloud } from "react-icons/fa";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import react, { useState, useEffect } from 'react';
-const Front = () => {
+import React, { useState } from 'react';
+const Front = (props) => {
   const apiKey = "b2dcbcd26cdccfeac192ff8f877e233f"
-  const [inputCity, setInputCity] = useState()
+  const [inputCity, setInputCity] = useState("Bareily")
   const [data, setData] = useState({
 
   })
 
   const getWeatherDetails = (cityName) => {
-    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=" + apiKey + ""
+    props.setProgress(30);
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + ""
+    props.setProgress(50);
     axios.get(apiURL).then((res) => {
-      console.log(res);
+
       setData(res.data)
-    }).catch((e)=>{
+      props.setProgress(100);
+    }).catch((e) => {
       console.log(e);
-      if(e)
-      alert("city not found")
+      if (e)
+        props.setProgress(100);
+        toast.error("city not found")
     })
   }
 
   const handleInputChange = (e) => {
     setInputCity(e.target.value);
+    props.setProgress(0);
   }
 
   const handleSearch = () => {
     getWeatherDetails(inputCity);
-    
+
   }
-
-  useEffect(() => {
-
-    getWeatherDetails("Bareilly")
-  }, [])
 
   return (
     <div className='full'>
@@ -43,21 +45,33 @@ const Front = () => {
           <h1>Weather App</h1>
         </div>
         <div>
-        <input type="text" placeholder='Enter city' onChange={handleInputChange}></input></div>
-        
-        <div className='button'><button  onClick={handleSearch}> search</button></div>
+          <input type="text" placeholder='Enter city' onChange={handleInputChange}></input></div>
+
+        <div className='button'><button onClick={handleSearch}> search</button></div>
         <h2 className='live-weather-condition'>Live Weather Condition</h2>
         <h2 className='temp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C </h2>
         <h2 className='cityName'>{data?.name},{data?.sys?.country}</h2>
         <div className='max'>
-          <div className='min'> <h3 className='max-temp '>Max.temp ||<br></br>{((data?.main?.temp_max) - 273.15).toFixed(2)  }°C</h3></div>
+          <div className='min'> <h3 className='max-temp '>Max.temp ||<br></br>{((data?.main?.temp_max) - 273.15).toFixed(2)}°C</h3></div>
           <div className='min'> <h3 className='max-temp min-temp'>Min.temp ||<br></br>{((data?.main?.temp_min) - 273.15).toFixed(2)}°C</h3></div>
           <div className='min'> <h3 className='max-temp humidity'>Humidity ||<br></br>{data?.main?.humidity}%</h3></div>
           <div className='min'> <h3 className='max-temp speed'>Speed<br></br>{data?.wind?.speed}mm/h</h3></div>
-          
 
+
+        </div>
       </div>
-    </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   )
 }
